5b2caeb2387d6f2620d37c9672f5174a0036bdcd
Louis <louishayotte@gmail.com>
2024-11-20 12:04:28 +0100

    added exercice 7 & 8

--- requetes.sql
+++ requetes.sql
@@ -30,3 +30,41 @@ FROM bataille bt
 JOIN lieu li ON li.id_lieu = bt.id_lieu
 ORDER BY STR_TO_DATE(bataille_date, "%d/%m/%y") DESC
 
+-- 6
+SELECT pot.nom_potion, SUM(ing.cout_ingredient * com.qte) AS cout
+FROM potion pot
+JOIN composer com ON com.id_potion = pot.id_potion
+JOIN ingredient ing ON ing.id_ingredient = com.id_ingredient
+GROUP BY pot.id_potion
+ORDER BY cout DESC
+
+
+-- 7
+SELECT ing.nom_ingredient, ing.cout_ingredient, com.qte
+FROM ingredient ing
+JOIN composer com ON com.id_ingredient = ing.id_ingredient
+WHERE com.id_potion = (
+	SELECT id_potion
+	FROM potion
+	WHERE nom_potion LIKE "Santé"
+)
+
+-- 8
+SELECT pr.nom_personnage, pc.qte
+FROM personnage pr
+JOIN prendre_casque pc ON pc.id_personnage = pr.id_personnage
+WHERE pc.id_bataille = (
+	SELECT id_bataille
+	FROM bataille
+	WHERE nom_bataille LIKE "Bataille du village gaulois"
+)
+HAVING pc.qte >= ALL(
+	SELECT pc.qte
+	FROM personnage pr
+	JOIN prendre_casque pc ON pc.id_personnage = pr.id_personnage
+	WHERE pc.id_bataille = (
+		SELECT id_bataille
+		FROM bataille
+		WHERE nom_bataille LIKE "Bataille du village gaulois"
+	)
+)
