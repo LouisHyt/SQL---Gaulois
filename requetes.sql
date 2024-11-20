51bbb52e587cf8e8f225a8ece4551532e267b8f9
Louis <louishayotte@gmail.com>
2024-11-20 11:24:48 +0100

    added exercice 4 & 5

--- requetes.sql
+++ requetes.sql
@@ -18,3 +18,15 @@ JOIN specialite sp ON sp.id_specialite = pr.id_specialite
 ORDER BY li.nom_lieu ASC, pr.nom_personnage
 
 -- 4
+SELECT sp.nom_specialite, COUNT(pr.id_personnage) AS total
+FROM specialite sp
+JOIN personnage pr ON pr.id_specialite = sp.id_specialite
+GROUP BY sp.id_specialite
+ORDER BY total DESC
+
+-- 5
+SELECT bt.nom_bataille, DATE_FORMAT(bt.date_bataille, "%d/%m/%y") AS bataille_date, li.nom_lieu
+FROM bataille bt
+JOIN lieu li ON li.id_lieu = bt.id_lieu
+ORDER BY STR_TO_DATE(bataille_date, "%d/%m/%y") DESC
+
